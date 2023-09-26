c22d0af8b0504c6ef6f7f3eaed98016effe9dd51
jacklion710 <jack.lion710@gmail.com>
2023-09-26 17:49:55 -0400

    Added background image and adjusted colors

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,6 +1,6 @@
 import { Flex, Heading, Text, VStack, Center, Button, Box, Collapse, useDisclosure } from "@chakra-ui/react";
-import { AiFillRobot } from 'react-icons/ai';
-import { FaCode, FaAngleDown, FaAngleUp } from 'react-icons/fa'; 
+import { FaMusic, FaHeadphones } from 'react-icons/fa'; 
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa'; 
 import { Helmet } from "react-helmet";
 
 const LandingPage = () => {
@@ -10,55 +10,55 @@ const LandingPage = () => {
     <Center 
       height="88vh" 
       bg="gray.50" 
-      bgImage="url('/images/Circuit.jpeg')"
+      bgImage="url('/images/red-note.jpg')" 
       bgPos="center"
       bgSize="cover"
       bgRepeat="no-repeat"
     >
       <Helmet>
-        <title>Jacob Leone | AI Specialist & Software Engineer</title>
-        <meta name="description" content="Welcome to Jacob Leone's professional portfolio. Discover innovations by an AI Specialist and Software Engineer." />
-        <meta name="keywords" content="Jacob Leone, AI Specialist, Software Engineer, Tech Innovations" />
+        <title>Jacob Leone | Music Producer & Artist</title>
+        <meta name="description" content="Discover the musical world of Jacob Leone. From rock and pop to electronic vibes, feel the rhythm." />
+        <meta name="keywords" content="Jacob Leone, Music Producer, Electronic Artist, Music Portfolio" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Jacob Leone | AI Specialist & Software Engineer" />
-        <meta property="og:description" content="Welcome to Jacob Leone's professional portfolio. Discover innovations by an AI Specialist and Software Engineer." />
-        <meta property="og:url" content="https://jacobleone-tech.vercel.app/" />
-\        <link rel="canonical" href="https://jacobleone-tech.vercel.app/" />
+        <meta property="og:title" content="Jacob Leone | Music Producer & Artist" />
+        <meta property="og:description" content="Discover the musical world of Jacob Leone. From rock and pop to electronic vibes, feel the rhythm." />
+        <meta property="og:url" content="https://jacobleone-music.vercel.app/" />
+        <link rel="canonical" href="https://jacobleone-music.vercel.app/" />
       </Helmet>
-      <Box bgColor="rgba(230, 230, 230, 0.7)" p={6} borderRadius="md">
+      <Box bgColor="rgba(0, 0, 0, 0.7)" p={8} borderRadius="md">
         <VStack spacing={6}>
           <Heading 
             fontSize={['2xl', '3xl', '4xl', '5xl']} 
-            color="teal.500"
+            color="white"
           >
             Jacob Leone
           </Heading>
           <VStack spacing={2}>
-            <AiFillRobot size="1.5em" />
-            <Text fontSize={['lg', 'xl', '2xl']} color="gray.700">AI Specialist</Text>
+            <FaMusic size="1.5em" color="red.500" />
+            <Text fontSize={['lg', 'xl', '2xl']} color="gray.300">Music Producer</Text>
           </VStack>
           <VStack spacing={2}>
-            <FaCode size="1.5em" />
-            <Text fontSize={['lg', 'xl', '2xl']} color="gray.700">Software Engineer</Text>
+            <FaHeadphones size="1.5em" color="red.500" />
+            <Text fontSize={['lg', 'xl', '2xl']} color="gray.300">Electronic Artist</Text>
           </VStack>
           <Button 
-              colorScheme="teal" 
+              colorScheme="red" 
               size="lg" 
               variant="outline" 
-              onClick={() => window.location.href="/Projects"}
+              onClick={() => window.location.href="/Tracks"}
           >
-              Discover My Innovations
+              Listen to My Reel
           </Button>
           <Box textAlign="center" w="100%" my={4}>
-            <Text fontSize="lg" color="teal.500" mb={1} maxW="700px" textAlign="center" mx="auto">
-              I am driven by an intrinsic passion for tackling both creative and technical challenges. As part of agile development teams, I thrive on collaborating towards our shared mission: delivering exceptional value to clients efficiently and effectively...
+            <Text fontSize="lg" color="gray.300" mb={1} maxW="700px" textAlign="center" mx="auto">
+              At the intersection of technology and artistry, I leverage my expertise to provide creative solutions to brands in the musical sphere.
             </Text>
-            <Button variant="link" color="teal.500" onClick={onToggle} mt={2}>
+            <Button variant="link" color="red.500" onClick={onToggle} mt={2}>
               {isOpen ? "Read Less" : "Read More"} {isOpen ? <FaAngleUp /> : <FaAngleDown />}
             </Button>
             <Collapse in={isOpen}>
-              <Text fontSize="lg" color="teal.500" mt={4} maxW="700px" textAlign="center" mx="auto">
-                Before diving into the tech arena, I pursued an intensive graduate program at a top-tier university, focusing on the convergence of artificial intelligence and psychology. This academic journey endowed me with a unique lens: approaching AI development from a human-centric perspective and deeply understanding the nuanced ways people interact with technology. Beyond my academic pursuits, I am a perpetual learner, drawing from experiences that span leadership, creativity, problem-solving, and shepherding projects from mere ideas to successful execution.
+              <Text fontSize="lg" color="gray.300" mt={4} maxW="700px" textAlign="center" mx="auto">
+                Through a combination of innovative sound design and adaptive production techniques, I partner with brands to curate unique audio experiences tailored to their needs. Understanding a brand's vision and translating it into memorable music compositions is my forte. Whether it's creating sonic identities for new products, enhancing media campaigns, or simply producing tracks that resonate with target audiences, I'm dedicated to delivering exceptional value and results.
               </Text>
             </Collapse>
           </Box>
